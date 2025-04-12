fc45c4b7d4df4e28ee06492b847b5b404090bccf
cemen <frodo.work@gmail.com>
2025-04-12 12:42:14 +0300

    added admin settings

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from reservation.models import Visitor, Reservation, City, Cafe, Table
+
+
+class TableAdmin(admin.ModelAdmin):
+    list_display = ("number", "cafe", "seats", "is_reserved")
+    list_filter = ("cafe",)
+    search_fields = ("number", "cafe__name")
+    ordering = ("cafe", "seats")
+
+
+admin.site.register(Visitor)
+admin.site.register(Table, TableAdmin)
+admin.site.register(Cafe)
+admin.site.register(City)
+admin.site.register(Reservation)
