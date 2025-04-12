0bbc21b43e7a0a4f3b873ad1b77789a85f97d91e
cemen <frodo.work@gmail.com>
2025-04-12 15:22:00 +0300

    refactored views (added city) and cafe_list.html (added filter, changed layout)

--- reservation/views.py
+++ reservation/views.py
@@ -6,24 +6,25 @@ from django.views.generic import ListView
 from reservation.models import Cafe, City
 
 
+# class CafeListView(ListView):
+#     model = Cafe
+#     template_name = "reservation/cafe_list.html"
+#     context_object_name = "cafes"
+
+
+# class CafeListView(LoginRequiredMixin, ListView):
 class CafeListView(ListView):
     model = Cafe
     template_name = "reservation/cafe_list.html"
     context_object_name = "cafes"
 
+    def get_queryset(self) -> QuerySet:
+        queryset = Cafe.objects.all()
+        if self.request.user.city:
+            queryset = queryset.filter(city=self.request.user.city)
+        return queryset
 
-# class CafeListView(LoginRequiredMixin, ListView):
-#     model = CafeListView
-#     template_name = "reservation/cafe_list.html"
-#     context_object_name = "cafes"
-#
-#     def get_queryset(self) -> QuerySet:
-#         queryset = Cafe.objects.filter(is_active=True)
-#         if self.request.user.city:
-#             queryset = queryset.filter(city=self.request.user.city)
-#         return queryset
-#
-#     def get_context_data(self, *kwargs) -> dict:
-#         context = super().get_context_data(**kwargs)
-#         context["cities"] = City.objects.all().order_by("name")
-#         return context
+    def get_context_data(self, **kwargs) -> dict:
+        context = super().get_context_data(**kwargs)
+        context["cities"] = City.objects.all().order_by("name")
+        return context
